2916603a0b76698a237ab2f6c79a2df2388fb6a0
Joey Fang <josudoey@gmail.com>
2017-03-09 23:36:01 +0800

    [add] check disk script

---
+++ check/disk/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+prefix=${repo:-https://github.com/josudoey/consul-script/raw/master}
+if [ -x /usr/local/bin/check-disk ]; then
+curl -fLo /usr/local/bin/check-disk $prefix/check/disk/check-disk
+chmod +x /usr/local/bin/check-disk
+fi
+curl -fLo /etc/consul.d/disk.json $prefix/check/disk/disk.json
