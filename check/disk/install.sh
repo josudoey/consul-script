a5a9a4fc6b210314c0aad333fa5cc46422fe30a4
Joey Fang <josudoey@gmail.com>
2017-03-09 23:55:05 +0800

    [modify] patch check disk

--- check/disk/install.sh
+++ check/disk/install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 prefix=${repo:-https://github.com/josudoey/consul-script/raw/master}
-if [ ! -x /usr/local/bin/check-disk ]; then
- curl -fLo /usr/local/bin/check-disk $prefix/check/disk/check-disk
- chmod +x /usr/local/bin/check-disk
-fi
-test -e /etc/consul.d/disk.json || curl -fLo /etc/consul.d/disk.json $prefix/check/disk/disk.json
-
+curl -fLo /usr/local/bin/check-disk $prefix/check/disk/check-disk
+chmod +x /usr/local/bin/check-disk
+curl -fLo /etc/consul.d/disk.json $prefix/check/disk/disk.json
 echo "===you need reload consul==="
 echo "consul reload"
